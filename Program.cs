8af6ec81019e178751dc43c4476ddc4483f76754
Vinay <navik.vinay12@gmail.com>
2022-11-25 21:47:29 +0530

    Initial commit-only project created,not added anyCode

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LinkedListUCs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LinkedList UCs");
+        }
+    }
+}
